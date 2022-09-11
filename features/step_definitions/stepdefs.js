66e65981aa8900a24b4462beb2a3fe4ac9c8046f
RVLareu <rlareu@fi.uba.ar>
2022-09-11 20:42:51 -0300

    minor fix de pinta a los steps

--- features/step_definitions/stepdefs.js
+++ features/step_definitions/stepdefs.js
@@ -2,22 +2,20 @@ const assert = require('assert');
 const { Given, When, Then } = require('@cucumber/cucumber');
 
 function isShipmentFree(client, amountOfBooks) {
-    if (client === "VIP" && amountOfBooks >= 5) {
-      return "free";
-    } else {
-      return "charged";
-    }
+  if (client === "VIP" && amountOfBooks >= 5) {
+    return "free";
+  } else {
+    return "charged";
   }
-  
+}
 
-Given ('the client is {string}', function (client) {
+  Given ('the client is {string}', function (client) {
     this.client = client;
 })
 
 Given ('the client bought {int}', function (amountOfBooks) {
   this.amountOfBooks = amountOfBooks;
 })
-
 
 When('the client wants to pay for the cart', function () {
   this.shipment = isShipmentFree(this.client, this.amountOfBooks);
